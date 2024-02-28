09d39165b871d80e82aa7183ddd67502ba7b23a6
Erik <erikpersson0884@gmail.com>
2024-02-28 14:57:02 +0100

    added ability to change update times

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -2,11 +2,21 @@ const imageContainer = document.getElementById('imageContainer');
 const imageCarousel = document.getElementById('imageCarousel');
 const optionsMenu = document.getElementById('optionsMenu');
 
+const settingsDiv = document.getElementById('settingsDiv');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const carouselSpeedInput = document.getElementById('carouselSpeedInput');
+
 
 const pathToEventImages = '/img/eventImages/';
-const autoRefreshTime = 24 * 60 * 60 * 1000; //milliseconds
+
 let currentIndex = 0;
 let images = [];
+
+let autoRefreshTime = 60 * 1000; //milliseconds
+let carouselSpeed = 8 * 1000; //milliseconds
+let timeBeforeHidingMenus = 5 * 1000; //milliseconds
 
 
 imageCarousel.onerror = function() {
@@ -24,7 +34,7 @@ function displayNextImage() {
         imageCarousel.src = pathToEventImages + currentImage.path;
         currentIndex = (currentIndex + 1) % images.length;
 
-        setTimeout(displayNextImage, 5000);
+        setTimeout(displayNextImage, carouselSpeed);
     } else {
         images.splice(currentIndex, 1);
 
@@ -45,9 +55,32 @@ function fetchUpcomingImages() {
 
 fetchUpcomingImages();
 
+
 // Automatically refresh the page
 setTimeout(function() {window.location.reload(true);}, autoRefreshTime);
 
 // hide optionsMenu after a few seconds
-setTimeout(function() {optionsMenu.classList.add('invisible')}, 5 * 1000);
+setTimeout(function() {
+    optionsMenu.classList.add('invisible')
+    settingsDiv.classList.add('invisible')
+}, timeBeforeHidingMenus);
 
+carouselSpeedInput.value = carouselSpeed / 1000;
+
+[hours, minutes, seconds].forEach(time => {
+    time.addEventListener('change', function() {
+        autoRefreshTime = hours.value * 60 * 60 * 1000 + minutes.value * 60 * 1000 + seconds.value * 1000;
+        if (autoRefreshTime < (10 * 1000)) {
+            autoRefreshTime = 10000;
+        }
+        console.log("Auto refresh time:", autoRefreshTime);
+    });
+});
+
+carouselSpeedInput. addEventListener('change', function() {
+    carouselSpeed = carouselSpeedInput.value * 1000;
+    if (carouselSpeed < 1000) {
+        carouselSpeed = 1000;
+    }
+    console.log("Carousel speed:", carouselSpeed);
+});
